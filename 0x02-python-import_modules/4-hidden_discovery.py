af13fb46db348ff0e5d662fc174cc0e4c913db0a
Ntuthuko <ntuthukolu@gmail.com>
2023-05-11 18:58:48 -0700

    4 hidden discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import sys
-import hidden_4 as hidden
+import hidden_4
 if __name__ == "__main__":
-    exit()
+    name = dir(hidden_4)
 
-for name in dir(hidden):
-    if name [:2] != "__":
-        print(name)
+for _name in name:
+    if _name [0:2] != "__":
+        print(_name)
