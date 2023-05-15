7b0c92c33af2c4d5e163896f4d54decabf4efd63
Ntuthuko <ntuthukolu@gmail.com>
2023-05-15 14:32:26 -0700

    Replace

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if my_list:
+        new_list = []
+        for num in my_list:
+            new_list.append(num)
+        if not idx < 0 and not idx >= len(my_list):
+            new_list[idx] = element
+        return 
