e9986db10006803c2c130bf82d3740bc682d5c80
Ntuthuko <ntuthukolu@gmail.com>
2023-05-29 12:51:22 -0700

    print and count

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    """
+    Prints the first x elements of a list and only integers.
+
+    Parameters:
+    my_list (list): Contains any type (integer, string, etc.)
+    x (int): Number of elements to access in my_list
+
+    Returns:
+    int: The real number of integers printed
+
+    Raises:
+    Exception: If x is greater than the length of my_list
+    """
+    elements_printed = 0
+    for element in my_list[:x]:
+        try:
+            print("{:d}".format(element), end="")
+            elements_printed += 1
+            print()
+        except (TypeError, ValueError):
+            pass
+    return elements_printed
