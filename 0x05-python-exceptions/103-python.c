4d97ea6741c78494e432b55b14638e034430f9e8
Ntuthuko <ntuthukolu@gmail.com>
2023-05-29 13:46:44 -0700

    c

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <Python.h>
 
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
 /**
  * print_python_list - prints some basic info about Python lists
  * @p: Pointer to the Python object
