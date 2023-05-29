eaac489ea3eab53a8b3b536e17721626b7bb4e0d
Ntuthuko <ntuthukolu@gmail.com>
2023-05-29 13:28:40 -0700

    bonus task1

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+def safe_print_integer_err(value):
+    """
+    Prints an integer value followed by a new line.
+    Returns True if value is an integer and False otherwise.
+    If False, prints an error message preceded by "Exception:" to stderr.
+
+    Parameters:
+    value (any type): Value to be printed
+
+    Returns:
+    bool: True if value is an integer, False otherwise
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except:
+        print("Exception:", sys.exc_info()[0])
+        return False
