836b59cae1c543ba6b7fc4adc466ab2d7777307f
Ntuthuko <ntuthukolu@gmail.com>
2023-05-30 20:27:07 -0700

    the area

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """ Clases defines a square"""
+    def __init__(self, size=0):
+        """the initialization of a square and size"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Prints area"""
+        return self.__size ** 2
