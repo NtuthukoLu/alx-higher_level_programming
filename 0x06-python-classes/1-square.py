0386e89fec5e6ce9d236a6267bf53528a9d1e905
Ntuthuko <ntuthukolu@gmail.com>
2023-05-30 20:21:55 -0700

    defines a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This is the square class!"""
+class Square:
+    """Class defines a square"""
+    def __init__(self, size=0):
+        """This initializes a square with a size"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
