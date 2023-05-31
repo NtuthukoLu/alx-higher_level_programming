7eb5a28de258140d16a504f7c60788c14d3e8b0a
Ntuthuko <ntuthukolu@gmail.com>
2023-05-31 08:58:49 -0700

    bonus task 1

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -19,5 +19,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except:
-        print("Exception: {}".format(sys.exc_info()[0]), file=sys.stderr)
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return False
