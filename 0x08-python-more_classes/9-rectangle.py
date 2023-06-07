4140ee5a1db3e282023436de9c245abcbaf7093e
Ntuthuko <ntuthukolu@gmail.com>
2023-06-08 00:19:12 +0200

    class method

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -85,4 +85,4 @@ class Rectangle():
     @classmethod
     def square(cls, size=0):
         """returns a new rectangle width and height"""
-        return (cls(size, size))
+        return cls(size, size)
