5d75cae17d2377fba8df49eb93d8cdcf2c2b8d02
Ntuthuko <ntuthukolu@gmail.com>
2023-06-13 00:28:07 +0200

    task6

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
+"""6-base_geometry module"""
 
 
 class BaseGeometry():
-    """for use with shapes. Super class.
-    """
-
+    """for use with shapes. Super class. """
     def area(self):
-        """instance method to calculate area of shape
-        """
+        """instance method to calculate area of shape"""
         raise Exception("area() is not implemented")
