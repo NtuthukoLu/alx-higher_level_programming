7b068855a3144243b5ffbbd8859085a793c349fb
Ntuthuko <ntuthukolu@gmail.com>
2023-06-12 23:54:36 +0200

    same class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" 2-is_same_class.py Module """
+
+
+def is_same_class(obj, a_class):
+    """checks if two objects are EXACTLY the same clas"""
+    return type(obj) == a_class
