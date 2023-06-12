e4b6cbb892c190d3dc00ffd66bb9cdc37c670454
Ntuthuko <ntuthukolu@gmail.com>
2023-06-13 01:44:31 +0200

    101

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""101 add attribute module"""
+
+
+def add_attribute(obj, name, value):
+    """adds an attribute to the class if possible
+        -> aka is
+    """
+    builtins = (str, int, complex, bool, float, list,
+                tuple, dict, set, frozenset, type, object)
+    for _type in builtins:
+        if type(obj) is _type:
+            raise TypeError("can't add new attribute")
+
+    object.__setattr__(obj, name, value)
