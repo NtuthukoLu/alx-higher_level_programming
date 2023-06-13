255283bd658f69f422ac85eb269c1509715cc607
Ntuthuko <ntuthukolu@gmail.com>
2023-06-13 18:58:29 +0200

    task0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""0-read_file module"""
 
 
 def read_file(filename=""):
     """Reads a file"""
-    with open(filename, encoding='utf-8') as myfile:
-        print(myfile.read(), end='')
+    with open(filename, encoding="utf-8") as myfile:
+        print(myfile.read(), end="")
