5b1759f08d7d9ac4e7bb240521d0afbe16503716
Ntuthuko <ntuthukolu@gmail.com>
2023-06-13 20:41:55 +0200

    add

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,7 +7,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 fileName = "add_item.json"
 List = []
 
-try: 
+try:
     List = load_from_json_file(fileName)
 except:
     pass
