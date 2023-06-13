5aa30decfc92e7545e967a0a4d12298593ec7035
Ntuthuko <ntuthukolu@gmail.com>
2023-06-13 20:59:28 +0200

    stud2

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""10-student Module"""
+
+
+class Student:
+    """Student Class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieves a dictionary """
+        dictionary vars(self)
+        if attrs is None:
+            return dictionary
+
+        studInfo = {}
+        for item in attrs:
+            if item in dictionary:
+                studInfo[item] = dictionary[item]
+        return studInfo
+~                                                                                                
+~                               
