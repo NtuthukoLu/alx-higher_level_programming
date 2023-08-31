76c0924936ccb0453bcfedce1fe95e47eaf4bd08
Ntuthuko <ntuthukolu@gmail.com>
2023-08-31 18:46:33 +0200

    task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,12 @@
 #!/usr/bin/node
-const parsed = parseInt(process.argv[2]);
-if (parsed) {
-  console.log('My number:', parseInt(process.argv[2]));
-} else {
-  console.log('Not a number');
+const parsed = require('process');
+let resp;
+let num;
+resp = 'Not a number';
+if (parsed.argv.length > 2) {
+  num = parseInt(parsed.argv[2]);
+  if (!isNaN(num)) {
+    resp = 'My number: ' + String(num);
+  }
 }
+console.log(resp);
