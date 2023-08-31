9a67e9bf43a7211ee138e89a63975d615198e80c
Ntuthuko <ntuthukolu@gmail.com>
2023-08-31 22:31:58 +0200

    silent curls

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
-
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-url=$1
-
-# Send a request using curl and save the response body to a temporary file
-response=$(curl -sI "$url")
-temp_file=$(mktemp)
-curl -s "$url" -o "$temp_file"
-
-# Get the size of the response body in bytes
-size=$(wc -c < "$temp_file")
-
-# Clean up the temporary file
-rm "$temp_file"
-
-echo "$size"
+# To get the body size of a request
+curl -Is "$1" | grep -w 'Content-Length' | cut -f2 -d' '
