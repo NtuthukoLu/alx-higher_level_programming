e1b528246c0d3b24bdd8bf3cd6af48153fd22153
Ntuthuko <ntuthukolu@gmail.com>
2023-09-02 02:59:27 +0000

    new changes

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
+"""script for testing status of web pages
 """
-script that fetches `https://intranet.hbtn.io/status`.
-"""
-from urllib import request
-
 if __name__ == "__main__":
-    req = request.Request('https://intranet.hbtn.io/status')
-    with request.urlopen(req) as response:
-        html = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(html.decode('utf8')))
+    import urllib.request
+    url = "https://intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        bytes_content = response.read()
+        content = bytes_content.decode('utf-8')
+        print_str = '''Body response:
+\t- type: {}
+\t- content: {}
+\t- utf8 content: {}'''.format(type(bytes_content), bytes_content, content)
+        print(print_str)
