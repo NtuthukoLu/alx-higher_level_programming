4206a2fc0d573cc6fc7dec108332bf34a49ff65d
Ntuthuko <ntuthukolu@gmail.com>
2023-09-02 02:52:18 +0000

    initial commit

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+script that fetches `https://intranet.hbtn.io/status`.
+"""
+from urllib import request
+"""
+
+if __name__ == "__main__":
+    req = request.Request('https://intranet.hbtn.io/status')
+    with request.urlopen(req) as response:
+        html = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode('utf8')))
