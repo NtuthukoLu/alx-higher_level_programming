c1e1b509961810040a5a1bb03b6e983006981013
Ntuthuko <ntuthukolu@gmail.com>
2023-09-05 20:16:20 +0200

    insert

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -2,25 +2,24 @@
 """
 Script to list all states in the states table.
 """
-from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+
+from sqlalchemy.orm import sessionmaker
+
 from sqlalchemy import (create_engine)
+
 import sys
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    # create custom session object class from database engine
     Session = sessionmaker(bind=engine)
+    # create instance of new custom session class
     session = Session()
-
-    session.add(State(name="Louisiana"))
-    state = session.query(State)\
-        .filter(State.name == "Louisiana")\
-        .order_by(State.id)[-1]
-    print(state.id)
-
+    new_state = State()
+    new_state.name = 'Louisiana'
+    session.add(new_state)
     session.commit()
+    print(new_state.id)
