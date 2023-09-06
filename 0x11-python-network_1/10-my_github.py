012d72bb153d0f1aa5df2e400f9bffcd6f956633
Ntuthuko <ntuthukolu@gmail.com>
2023-09-06 02:09:24 +0200

    github

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -3,9 +3,21 @@ import requests
 from requests.auth import HTTPBasicAuth
 import sys
 
-if __name__ == "__main__":
-    url = 'https://api.github.com/user'
-    username = sys.argv[1]
-    token = sys.argv[2]
-    http = requests.get(url, auth=HTTPBasicAuth(username, token)).json()
-    print(http.get('id'))
+if len(sys.argv) != 3:
+    print("Usage: ./10-my_github.py <username> <personal_access_token>")
+    sys.exit(1)
+
+username = sys.argv[1]
+access_token = sys.argv[2]
+url = "https://api.github.com/user"
+
+try:
+    response = requests.get(url, auth=(username, access_token))
+    if response.status_code == 200:
+        data = response.json()
+        print(data["id"])
+    else:
+        print(None)
+
+except requests.exceptions.RequestException as e:
+    print(f"Error: {e}")
