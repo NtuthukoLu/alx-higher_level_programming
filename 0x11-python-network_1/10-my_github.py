612327ab0c7a8e183da0b9bc897112dfc91f24fe
Ntuthuko <ntuthukolu@gmail.com>
2023-09-06 02:07:16 +0200

    pycodestyle

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-"""
-takes your GitHub credentials (username and password) and uses the GitHub API to display your id
-"""
-
 import requests
 from requests.auth import HTTPBasicAuth
 import sys
